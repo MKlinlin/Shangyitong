45969de726cf475fa7c0817867923bfbbd7319b2
MKlinlin <3024672738@qq.com>
2024-03-13 14:51:13 +0800

    顶部组件搭建

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <!-- 顶部全局组件 -->
+    <HospitalTop />
+    <!-- 医院信息 -->
+    <div class="content">
+      xxxxxx
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"> 
+</script> 
+
+
+
+<style scoped lang="scss">
+.container{
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  .content{
+    margin-top: 70px;
+    width: 1300px;
+    min-height: 700px;
+    background-color: red;
+  }
+}
+
+</style>
---
+++ src/components/hospital_top/index.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="top">
+        <div class="content">
+            <!-- 左侧 -->
+            <div class="left">
+                <img src="../../assets/images/logo.png" alt="">
+                <p>尚医通 预约挂号统一平台</p>
+            </div>
+            <div class="right">
+                <p class="help">帮助中心</p>
+                <p class="login">登录/注册</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.top{
+position:fixed;
+z-index: 99;
+width: 100%;
+height: 70px;
+display: flex;
+justify-content: center;
+.content{
+    width: 1300px;
+    height: 100%;
+    margin: 0 auto;
+    background-color:#fff;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .left{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        img{
+            width: 50px;
+            height: 50px;
+            margin-right: 10px;
+        }
+        p{
+            font-size: 20px;
+            color: #55a6fe;
+        }
+        
+    }
+    .right{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 12px;
+        .help{
+            margin-right:10px
+        }
+    }
+}
+}
+</style>
