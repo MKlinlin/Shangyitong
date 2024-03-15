f67d298695b2934454ba58d6fb489e8e8ad62dee
MKlinlin <3024672738@qq.com>
2024-03-15 15:57:29 +0800

    引入elment-plus及其图标库

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import '@/style/reset.scss'
+import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 //引入全局组件
 import HospitalTop from '@/components/hospital_top/index.vue'
 import HospitalBottom from '@/components/hospital_bottom/index.vue'
-import router from './router'
+
 const app = createApp(App)
 
 app.component('HospitalTop', HospitalTop)
 app.component('HospitalBottom', HospitalBottom)
-
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
 app.use(router)
+app.use(ElementPlus)
 //挂载
 app.mount('#app')
