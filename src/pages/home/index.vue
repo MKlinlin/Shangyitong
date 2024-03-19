777e22d0b69fe9e61e6a2e5127570983b8bec067
MKlinlin <3024672738@qq.com>
2024-03-19 10:24:08 +0800

    等级与地区组件静态搭建

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -4,12 +4,23 @@
     <Carousel />
     <!-- 搜索区 -->
     <Search />
+    <el-row gutter="20">
+      <el-col :span="20">
+        <Level />
+        <Region />
+      </el-col>
+      <el-col :span="4">
+        123
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script setup lang="ts">
 import Carousel from "./carousel/index.vue";
 import Search from "./search/index.vue";
+import Level from "./level/index.vue";
+import Region from "./region/index.vue";
 </script>
 
 <style lang="scss" scoped>
---
+++ src/pages/home/region/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="region">
+    <div class="content">
+       <div class="left">地区:</div>
+        <ul>
+            <li class="active">全部</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+        </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss" >
+.region{
+    color: #7f7f7f;
+    .content{
+        display:flex ;
+        .left{
+            
+            width: 50px;
+        }
+        ul{
+            display: flex;
+            flex-wrap: wrap;
+            li{
+                margin-right: 10px;
+                margin-bottom: 10px;
+                cursor: pointer;
+                &.active{
+                    color: #55a6f5;
+                }
+            }
+            li:hover{
+                color: #55a6f5;
+            }
+
+        }
+    }
+}
+</style>
