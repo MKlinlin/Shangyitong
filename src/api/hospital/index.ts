7e9797106ced024e815fcf95524607c2a4b0217a
MKlinlin <3024672738@qq.com>
2024-03-26 17:04:39 +0800

    登录组件获取验证码

--- src/api/hospital/index.ts
+++ src/api/hospital/index.ts
@@ -4,6 +4,8 @@ enum API {
     HOSPITALDETAIL_URL = '/hosp/hospital/',
     //获取某个医院的科室数据
     HOSPITALDEPARTMENT_URL= '/hosp/hospital/department/',
+    //获取验证码
+    GETUSERCODE_URL = '/sms/send/',
 }
 
 // 获取医院详情的接口
@@ -11,3 +13,6 @@ export const reqHospitalDetail = (hoscode: string) => request.get<any, HosPitalD
 
 // 获取某个医院科室接口
 export const reqHospitalDepartment = (hoscode: string) => request.get<any, DeparmentResponseData>(API.HOSPITALDEPARTMENT_URL + hoscode);
+
+// 获取验证码接口
+export const reqCode = (phone: string) => request.get<any, string>(API.GETUSERCODE_URL + phone);
--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,14 +1,24 @@
 //定义用户相关仓库
 import { defineStore } from "pinia";
-
+import { reqCode } from "@/api/hospital";
 const useUserStore = defineStore('User',{
     state:()=>{
         return {
             visiable: false, //登录组件dialog显示与隐藏
+            code:'', //验证码
         }
     },
     actions:{
-      
+      //获取验证码方法
+      async getCode(phone:string){
+      let result:any  =  await reqCode(phone)
+        if (result.code === 200) {
+            this.code = result.data
+            console.log(this.code);
+        }else{
+            return Promise.reject(new Error('获取验证码失败'))
+        }
+      }
     },
     getters:{
       
